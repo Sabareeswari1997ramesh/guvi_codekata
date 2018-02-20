f7fafbbae9913ccd6201055e986d5cd849ba69bd
Sabareeswari1997ramesh <36658979+Sabareeswari1997ramesh@users.noreply.github.com>
2018-02-20 14:38:00 +0530

    Add files via upload

---
+++ sabari.py
@@ -0,0 +1,8 @@
+x=int(raw_input("Enter the Number:"))
+if((0<x<1000000)|(-1000000<x<1)):
+    if(x>0):
+        print("Positive")
+    elif(x<0):
+        print("Negative")
+    else:
+        print("Zero")

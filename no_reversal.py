0e54dcd2fd5bcace706d6fcf1812c20b95d71c24
Sabareeswari1997ramesh <36658979+Sabareeswari1997ramesh@users.noreply.github.com>
2018-05-16 15:01:39 +0530

    Add files via upload

---
+++ no_reversal.py
@@ -0,0 +1,35 @@
+x=int(raw_input())
+
+if((x>=0)&(x<10000)):
+
+    if(x<1000):
+
+        i=x/100
+
+        j=x%100
+
+        k=j/10
+
+        l=j%10
+
+        print(str(l)+str(k)+str(i))
+
+    elif(x>1000):
+
+        u=x/1000
+
+        v=x%1000
+
+        w=v/100
+
+        y=v%100
+
+        p=y/10
+
+        z=y%10
+
+        print(str(z)+str(p)+str(w)+str(u))
+
+else:
+
+    print("Enter the valid input")

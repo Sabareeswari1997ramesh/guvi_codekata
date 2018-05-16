0d11d77e8e5f055deb3c5fcb5648a2d8bf2805fc
Sabareeswari1997ramesh <36658979+Sabareeswari1997ramesh@users.noreply.github.com>
2018-05-16 12:50:09 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,20 @@
+a=int(raw_input())
+
+i=a
+
+j=1
+
+if(a<=20):
+   
+    while(i>=1):
+       
+        j=j*i
+
+        i=i-1
+
+    print(j)
+
+else:
+    
+    print("Enter the no with in the limit")
+    

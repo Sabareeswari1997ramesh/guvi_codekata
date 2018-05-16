7d17810ed99e16a2319b778c0716f3dde00a6910
Sabareeswari1997ramesh <36658979+Sabareeswari1997ramesh@users.noreply.github.com>
2018-05-16 13:47:09 +0530

    Add files via upload

---
+++ fact.py
@@ -0,0 +1,19 @@
+a=int(raw_input())
+
+i=a
+
+j=1
+
+if((a<=20)&(a>=0)):
+   
+    while(i>=1):
+       
+        j=j*i
+
+        i=i-1
+
+    print(j)
+
+else:
+    
+    print("Enter the no with in the limit")

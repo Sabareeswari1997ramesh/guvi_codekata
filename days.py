39ee3a76ef087061fea5dd8daa4168622ec34c14
Sabareeswari1997ramesh <36658979+Sabareeswari1997ramesh@users.noreply.github.com>
2018-05-16 15:34:36 +0530

    Add files via upload

---
+++ days.py
@@ -0,0 +1,17 @@
+y=raw_input()
+
+x=y.lower()
+
+if((x=='monday')|(x=='tuesday')|(x=='wednesday')|(x=='thursday')|(x=='friday')|(x=='saturday')|(x=='sunday')):
+
+    if((x=='monday')|(x=='tuesday')|(x=='wednesday')|(x=='thursday')|(x=='friday')):
+
+        print("no")
+
+    else:
+
+        print("yes")
+
+else:
+
+    print("Enter the valid input")

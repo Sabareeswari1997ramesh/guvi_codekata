5369429b10da9be0be3b5371ad87f9f9de73789d
Sabareeswari1997ramesh <36658979+Sabareeswari1997ramesh@users.noreply.github.com>
2018-05-17 09:32:01 +0530

    Add files via upload

---
+++ oddorev.py
@@ -0,0 +1,17 @@
+x=int(raw_input())
+
+if((x>=0)&(x<=100000)):
+
+    y=x%2
+
+    if(y==0):
+
+        print("Even")
+
+    else:
+
+        print("Odd")
+
+else:
+
+    print("Enter the valid input")

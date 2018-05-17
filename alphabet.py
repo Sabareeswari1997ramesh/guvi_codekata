495c7a5be4dbd458351a42a9806728bf2881ccb0
Sabareeswari1997ramesh <36658979+Sabareeswari1997ramesh@users.noreply.github.com>
2018-05-17 09:52:12 +0530

    Add files via upload

---
+++ alphabet.py
@@ -0,0 +1,15 @@
+x=raw_input()
+
+y=x.lower()
+
+if y in('aeiou'):
+
+    print("vowel")
+
+elif y in('bcdfghjklmnpqrstvwxyz'):
+
+    print("consonant")
+
+else:
+
+    print("Enter the valid input")

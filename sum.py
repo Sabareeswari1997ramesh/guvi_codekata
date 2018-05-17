a93fb3f415a907575899ca690b55a7a95e2b376e
Sabareeswari1997ramesh <36658979+Sabareeswari1997ramesh@users.noreply.github.com>
2018-05-17 10:21:31 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,21 @@
+try:
+
+    n=int(raw_input())
+
+except ValueError:
+
+    print("Enter only integer")
+
+else:
+
+    sum=0
+
+    i=n
+
+    while(i>0):
+
+        sum=sum+i
+
+        i=i-1
+
+    print(sum)    

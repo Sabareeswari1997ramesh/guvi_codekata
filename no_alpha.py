52a551c87ccdea7fb61cdc961c1506009aafdf7a
Sabareeswari1997ramesh <36658979+Sabareeswari1997ramesh@users.noreply.github.com>
2018-05-17 11:02:45 +0530

    Add files via upload

---
+++ no_alpha.py
@@ -0,0 +1,23 @@
+char=raw_input()
+
+y=char.lower()
+
+if y in('abcdefghijklmnopqrstuvwxyz'):
+
+    print("Alphabet")
+
+else:
+
+    try:
+
+        x=int(y)
+
+    except ValueError:
+
+        print("Enter the number or char only")
+
+    else:
+
+        print("No")
+    
+    

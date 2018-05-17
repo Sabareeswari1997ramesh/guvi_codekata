e53dec5121b0aaec2abf59afdcb0c26a91def6e8
Sabareeswari1997ramesh <36658979+Sabareeswari1997ramesh@users.noreply.github.com>
2018-05-17 10:31:51 +0530

    Add files via upload

---
+++ leapyear.py
@@ -0,0 +1,17 @@
+try:
+
+    n=int(raw_input())
+
+except ValueError:
+
+    print("Enter only year in 4 digit integer format")
+
+else:
+
+    if(n%4==0):
+
+        print("yes")
+
+    else:
+
+        print("no")

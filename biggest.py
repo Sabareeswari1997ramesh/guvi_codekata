222685046c356e13a6754455e2015abe0705896c
Sabareeswari1997ramesh <36658979+Sabareeswari1997ramesh@users.noreply.github.com>
2018-05-17 21:07:34 +0530

    Add files via upload

---
+++ biggest.py
@@ -0,0 +1,25 @@
+try:
+
+	x=int(raw_input())
+
+	y=int(raw_input())
+
+	z=int(raw_input())
+
+except ValueError:
+
+	print("Enter the integer value only")
+
+else:
+
+	if((x>y)&(x>z)):
+
+		print(x)
+
+	elif((y>x)&(y>z)):
+
+		print(y)
+
+	else:
+
+		print(z)

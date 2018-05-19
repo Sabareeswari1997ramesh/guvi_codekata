f324104db4890e9c7d3064594e321bafdd07664e
Sabareeswari1997ramesh <36658979+Sabareeswari1997ramesh@users.noreply.github.com>
2018-05-19 13:56:32 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,21 @@
+try:
+
+    x=int(raw_input())
+
+except ValueError:
+
+    print("Enter the integer only")
+
+else:
+
+    y=str(x)
+
+    z=y[::-1]
+
+    if(y==z):
+
+        print("yes")
+
+    else:
+
+        print("no")

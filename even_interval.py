edd0821881313b0f29fee86ba7c9140a521fa57e
Sabareeswari1997ramesh <36658979+Sabareeswari1997ramesh@users.noreply.github.com>
2018-05-19 13:54:45 +0530

    Add files via upload

---
+++ even_interval.py
@@ -0,0 +1,40 @@
+try:
+
+    x=int(raw_input())
+
+    y=int(raw_input())
+
+except ValueError:
+
+    print("enter integer inputs only")
+else:
+
+    if(x<y):
+
+        i=x+1
+
+        while(i<y):
+
+            if((i%2)==0):
+
+                print(i)
+
+            i=i+1
+
+    elif(x>y):
+
+        i=y+1
+
+        while(i<x):
+
+            if((i%2)==0):
+
+        	    print(i)
+
+            i=i+1
+
+    else:
+
+        print("Enter different integer inputs")
+    
+    

e3775c150dcc0efa24fd1618cf0bd95610da5c09
Sabareeswari1997ramesh <36658979+Sabareeswari1997ramesh@users.noreply.github.com>
2018-06-05 13:15:32 +0530

    Add files via upload

---
+++ listadd2.py
@@ -0,0 +1,23 @@
+list=[]
+
+n=int(raw_input())
+
+k=int(raw_input())
+
+for i in range(0,n):
+
+    no=int(raw_input())
+
+    list.append(no)
+
+sum=0
+
+i=0
+
+while(i<k):
+
+   sum=sum+list[i]
+
+   i=i+1
+
+print(sum)

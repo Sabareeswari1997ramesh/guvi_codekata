6cd7106aa7e608ddd0b368243d75853dcd469eed
Sabareeswari1997ramesh <36658979+Sabareeswari1997ramesh@users.noreply.github.com>
2018-06-05 13:34:43 +0530

    Add files via upload

---
+++ max_in_list.py
@@ -0,0 +1,19 @@
+list=[]
+
+n=int(raw_input())
+
+if(1<=n<=100000):
+
+    for i in range(0,n):
+
+        no=int(raw_input())
+
+        list.append(no)
+
+    print(max(list))
+
+else:
+
+    print("enter no within the limit")
+    
+	
